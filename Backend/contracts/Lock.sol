e96e895dc139edd1055255ee71b7f960ab3dffad
BikalpaRegmi <Bikalparegmihtd3@gmail.com>
2024-12-02 18:02:18 +0545

    make it better

--- Backend/contracts/Lock.sol
+++ Backend/contracts/Lock.sol
@@ -1,7 +1,52 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.27;
 
-contract NFTMARKETPLACE {
+import  "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-    
+contract NFTMARKETPLACE is ERC721URIStorage{
+    address payable public marketPlaceOwner;
+    uint256 listingFee = 0.001 ether;
+    uint private currentToken ;
+    uint private totalItemSold ;
+
+    struct NFTListing {
+        uint tokenId;
+        address payable owner;
+        address payable seller;
+        uint startPrice;
+        uint currentBid ;
+    }
+
+    mapping(uint=>NFTListing) private nft ;
+
+    constructor() ERC721("NFTMARKETPLACE" , "NFTS"){
+        marketPlaceOwner = payable(msg.sender);
+    }
+
+    modifier OnlyOwner {
+        require(msg.sender == marketPlaceOwner , "U R NOT THE OWNER");
+        _; 
+    }
+
+function getANft(uint _tokenId) external view returns (NFTListing memory) {
+    return nft[_tokenId];
+}
+
+function _createNftListing(uint _tokenId , uint _price) internal {
+
+}
+
+function createToken(string memory _tokenURI , uint _price) external returns(uint){
+require(_price>0 , "Price cant be zero");
+
+uint newTokenId = currentToken;
+_safeMint(msg.sender , newTokenId);
+_setTokenURI(newTokenId , _tokenURI);
+
+_createNftListing(newTokenId , _price);
+
+return newTokenId;
+}
+
 }
